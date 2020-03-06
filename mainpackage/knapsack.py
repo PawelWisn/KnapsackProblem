d7742439a00bcfc019eec376dfafb0d406bf2084
Pawel <wispawelwis38@gmail.com>
2020-03-06 12:18:39 +0100

    numpy added

--- mainpackage/knapsack.py
+++ mainpackage/knapsack.py
@@ -1,40 +1,53 @@
 from random import randint
 from math import floor
 import matplotlib.pyplot as plt
-
-
-class Individual:
-    def __init__(self, genes_num=None, genes=None):
-        if genes is None:
-            self.genes = [0 for _ in range(genes_num)]
-        else:
-            self.genes = genes
-
-    def mutate(self, rate):
-        genes_num = len(self.genes)
-        to_mutate_num = floor(genes_num * rate)
-        to_mutate = []
-        for i in range(to_mutate_num):
-            gene_idx = randint(0, genes_num - 1)
-            while gene_idx in to_mutate:
-                gene_idx = gene_idx + 1 if gene_idx < genes_num - 1 else 0
-            to_mutate.append(gene_idx)
-
-        for gene_idx in to_mutate:
-            self.genes[gene_idx] = int(not self.genes[gene_idx])
+import numpy as np
 
 
 class Population:
-    def __init__(self, pop_size=0, genes_num=None):
-        self.population = [Individual(genes_num) for _ in range(pop_size)]
+    def __init__(self, indivs=None):
+        if not indivs:
+            self.population = None
+            self.pop_size = 0
+        else:
+            self.population = np.array(indivs)
+            self.pop_size = self.population.shape[0]
 
-    def push_indiv(self, ind):
-        self.population.append(ind)
-
-    def best(self, task):
-        fitnesses = [fitness(indiv, task) for indiv in self.population]
-        index = fitnesses.index(max(fitnesses))
+    def get_by_idx(self, index):
         return self.population[index]
+
+    def gen_rand_pop(self, pop_size, genes_num):
+        self.pop_size = pop_size
+        pop_temp = []
+        for i in range(pop_size):
+            pop_temp.append([randint(0, 9) // 9 for _ in range(genes_num)])  # 90% for 0, 10% for 1
+        self.population = np.array(pop_temp)
+
+    def calc_fitness(self, task):
+        fitness_temp = []
+
+        values = np.array([t.c for t in task.items])
+        weights = np.array([t.w for t in task.items])
+        sizes = np.array([t.s for t in task.items])
+
+        sum_weights = self.population @ np.transpose(weights)
+        sum_sizes = self.population @ np.transpose(sizes)
+        sum_values = self.population @ np.transpose(values)
+
+        for i in range(self.pop_size):
+            if sum_sizes[i] > task.s or sum_weights[i] > task.w:
+                fitness_temp.append([0])
+            else:
+                fitness_temp.append([sum_values[i]])
+
+        self.fitness_arr = np.array(fitness_temp)
+
+    def get_rand_pop_slice(self, slice_size):
+        chosen_arr = np.random.choice([x for x in range(self.pop_size)], size=slice_size, replace=False)
+        return chosen_arr, np.array([self.fitness_arr[idx] for idx in chosen_arr])
+
+    def best(self):
+        return self.population[self.fitness_arr.argmax()]
 
 
 class Item:
@@ -99,74 +112,71 @@ def read_task(input_file):
         return task
 
 
-def fitness(individual, task):
-    max_w = task.w
-    max_s = task.s
+def mutate(genes, rate):
+    genes_num = len(genes)
+    to_mutate_num = floor(genes_num * rate)
+    to_mutate = np.random.choice([x for x in range(genes_num)], size=to_mutate_num, replace=False)
+    for gene_idx in to_mutate:
+        genes[gene_idx] = int(not genes[gene_idx])
+    return genes
+
+
+def fitness(genes, task):
     score = 0
     w_sum = 0
     s_sum = 0
-    for is_present, item in zip(individual.genes, task.items):
+    for is_present, item in zip(list(genes), task.items):
         if is_present:
             score += item.c
             w_sum += item.w
             s_sum += item.s
-            if w_sum > max_w or s_sum > max_s:
+            if w_sum > task.w or s_sum > task.s:
                 score = 0
                 break
     return score
 
 
-def tournament(population, tourn_size, task):
-    pop_size = len(population)
-    participants = []
-    for i in range(tourn_size):
-        candidate = randint(0, pop_size - 1)
-        while candidate in participants:
-            candidate = candidate + 1 if candidate < pop_size - 1 else 0
-        participants.append(candidate)
-
-    pop_slice = [population[participant] for participant in participants]
-    fitnesses = [fitness(indiv, task) for indiv in pop_slice]
-    max_fitness = max(fitnesses)
-    index = fitnesses.index(max_fitness)
-    return pop_slice[index]
+def tournament(population, tourn_size):
+    indexes, fitness_arr = population.get_rand_pop_slice(tourn_size)
+    best_idx = indexes[np.argmax(fitness_arr)]
+    return population.get_by_idx(best_idx)
 
 
-def crossover(parent1, parent2, rate):
+def crossover(parent1, parent2, rate): # single point method
     if randint(0, 1) / 100 > rate:
         return parent1
-    cutting_point = len(parent1.genes) // 3
-    child_genes = parent1.genes[:cutting_point] + parent2.genes[cutting_point:]
-    return Individual(genes=child_genes)
+    cutting_point = len(parent1) // 3
+    return list(parent1[:cutting_point]) + list(parent2[cutting_point:])
 
 
 if __name__ == '__main__':
-    POP_SIZE = 1000
-    TOURN_SIZE = 400
+    POP_SIZE = 700
+    TOURN_SIZE = 600
     CROSS_RATE = 0.4
     MUT_RATE = 0.01
-    ITERATIONS = 10
+    ITERATIONS = 250
     scores_per_gen = []
 
-    generate_task(n=1001, w=10001, s=10001, output_file='task.csv')
+    # generate_task(n=1001, w=10001, s=10001, output_file='task.csv')
     task = read_task(input_file='task.csv')
-    pop = Population(pop_size=POP_SIZE, genes_num=task.n)
+    pop = Population()
+    pop.gen_rand_pop(POP_SIZE, task.n)
     i = 0
     while i < ITERATIONS:
-        new_pop = Population()
+        new_pop = []
         j = 0
+        pop.calc_fitness(task)
         while j < POP_SIZE:
-            parent1 = tournament(pop.population, TOURN_SIZE, task)
-            parent2 = tournament(pop.population, TOURN_SIZE, task)
+            parent1 = tournament(pop, TOURN_SIZE)
+            parent2 = tournament(pop, TOURN_SIZE)
             child = crossover(parent1, parent2, CROSS_RATE)
-            child.mutate(MUT_RATE)
-            new_pop.push_indiv(child)
+            child = mutate(child, MUT_RATE)
+            new_pop.append(child)
             j += 1
-        pop = new_pop
-        best = pop.best(task)
+        best = pop.best()
+        pop = Population(new_pop)
         scores_per_gen.append(fitness(best, task))
-        print('Generation: ', i + 1, ', fitness: ', fitness(best, task), ', ones: ', sum(best.genes), ', genes: ',
-              best.genes, sep='')
+        print('Generation: ', i + 1, ', fitness: ', fitness(best, task), ', ones: ', sum(best), sep='')
         i += 1
     plt.plot([x for x in range(1, ITERATIONS + 1)], scores_per_gen)
     plt.xlabel('Generation')
